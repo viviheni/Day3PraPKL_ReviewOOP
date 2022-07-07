e22372b7c6ef2a813ece75b367ca8f910253be96
viviheni <vivi_058@smkassalaambandung.sch.id>
2022-07-07 14:25:08 +0700

    OOP

--- Guru/index.php
+++ Guru/index.php
@@ -45,7 +45,7 @@
                     <thead>
                         <tr>
                         <th scope="col">No</th>
-                        <th scope="col">NIP</th>
+                        <th scope="col">NIK</th>
                         <th scope="col">Nama Guru</th>
                         <th scope="col">Alamat</th>
                         <th scope="col">Action</th>
